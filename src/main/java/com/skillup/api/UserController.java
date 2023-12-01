c23205acf4d5761245ff1c7e50698592c96a6fea
neptunejing <jingnanqing@hotmail.com>
2023-12-01 23:02:33 +0200

    feat: add response body to prompt for duplicate userNames

--- src/main/java/com/skillup/api/UserController.java
+++ src/main/java/com/skillup/api/UserController.java
@@ -2,9 +2,12 @@ package com.skillup.api;
 
 import com.skillup.api.dto.in.UserInDto;
 import com.skillup.api.dto.out.UserOutDto;
+import com.skillup.api.util.SkillUpCommon;
+import com.skillup.api.util.SkillUpResponse;
 import com.skillup.domain.user.UserDomain;
 import com.skillup.domain.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +20,13 @@ public class UserController {
     UserService userService;
 
     @PostMapping(value = "/account")
-    public UserOutDto createUser(@RequestBody UserInDto userInDto) {
-        UserDomain userDomain = userService.createUser(toDomain(userInDto));
-        return toOutDto(userDomain);
+    public ResponseEntity<SkillUpResponse> createUser(@RequestBody UserInDto userInDto) {
+        try {
+            UserDomain userDomain = userService.createUser(toDomain(userInDto));
+            return ResponseEntity.status(SkillUpCommon.SUCCESS).body(SkillUpResponse.builder().result(toOutDto(userDomain)).build());
+        } catch (Exception e) {
+            return ResponseEntity.status(SkillUpCommon.BAD_REQUEST).body(SkillUpResponse.builder().msg(SkillUpCommon.USER_EXISTS).build());
+        }
     }
 
     private UserDomain toDomain(UserInDto userInDto) {
