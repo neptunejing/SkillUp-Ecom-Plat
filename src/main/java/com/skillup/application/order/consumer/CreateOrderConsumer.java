356ca49de9c31ba9b19e484bdb7edfdee78d2c27
neptunejing <jingnanqing@hotmail.com>
2024-04-09 11:35:12 +0300

    Revert "refactor: abstract CreateOrderEvent to decouple applications and infrastructure"
    This reverts commit ca394d5e37e300ffcb907bcf9dde8d2729978074.

--- src/main/java/com/skillup/application/order/consumer/CreateOrderConsumer.java
+++ src/main/java/com/skillup/application/order/consumer/CreateOrderConsumer.java
@@ -5,16 +5,20 @@ import com.skillup.application.order.MQSendRepo;
 import com.skillup.domain.order.OrderDomain;
 import com.skillup.domain.order.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @Slf4j
-public class CreateOrderEventHandler implements ApplicationListener<CreateOrderEvent> {
+@RocketMQMessageListener(topic = "${order.topic.create-order}", consumerGroup = "${order.topic.create-order-group}")
+public class CreateOrderConsumer implements RocketMQListener<MessageExt> {
     @Autowired
     OrderService orderService;
 
@@ -28,8 +32,9 @@ public class CreateOrderEventHandler implements ApplicationListener<CreateOrderE
     String payCheckTopic;
 
     @Override
-    public void onApplicationEvent(CreateOrderEvent event) {
-        OrderDomain orderDomain = event.orderDomain;
+    public void onMessage(MessageExt messageExt) {
+        String messageBody = new String(messageExt.getBody(), StandardCharsets.UTF_8);
+        OrderDomain orderDomain = JSON.parseObject(messageBody, OrderDomain.class);
         // 1. save an order to DB
         OrderDomain savedOrderDomain = orderService.getOrderById(orderDomain.getOrderNumber());
         if (!Objects.isNull(savedOrderDomain)) {
