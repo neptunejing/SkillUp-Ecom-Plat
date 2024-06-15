5352c2bd38fbd9c50cf33861de3a8bf752e6243b
neptunejing <jingnanqing@hotmail.com>
2024-06-15 15:12:24 +0300

    feat: create promotion-service module

--- promotion-service/src/main/java/com/skillup/domain/promotionCache/PromotionCacheService.java
+++ promotion-service/src/main/java/com/skillup/domain/promotionCache/PromotionCacheService.java
@@ -1,6 +1,5 @@
 package com.skillup.domain.promotionCache;
 
-import com.skillup.domain.promotionCache.PromotionCacheDomain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- promotion-service/src/main/java/com/skillup/infrastructure/repoImpl/JooqPromotionRepo.java
+++ promotion-service/src/main/java/com/skillup/infrastructure/repoImpl/JooqPromotionRepo.java
@@ -9,11 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
