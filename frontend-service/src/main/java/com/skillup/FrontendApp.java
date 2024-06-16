a61eac75c738cb9e0518dd2c08e50bdd10fb1fd7
neptunejing <jingnanqing@hotmail.com>
2024-06-16 21:55:37 +0300

    feat: create frontend-service module

--- frontend-service/src/main/java/com/skillup/FrontendApp.java
+++ frontend-service/src/main/java/com/skillup/FrontendApp.java
@@ -2,10 +2,12 @@ package com.skillup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class SkillUpApplication {
+@EnableDiscoveryClient
+public class FrontendApp {
     public static void main(String[] args) {
-        SpringApplication.run(SkillUpApplication.class, args);
+        SpringApplication.run(FrontendApp.class, args);
     }
 }
