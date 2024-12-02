bb9d51ae65b693b3d058a0d890ff74b89fbbbe74
neptunejing <jingnanqing@hotmail.com>
2024-12-02 14:04:45 +0200

    feat: order-service 支持 mybatis-plus

--- order-service-2/src/main/java/com/skillup/domain/order/OrderService.java
+++ order-service-2/src/main/java/com/skillup/domain/order/OrderService.java
@@ -1,6 +1,7 @@
 package com.skillup.domain.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class OrderService {
     @Autowired
+    @Qualifier("batisOrderRepo")
     OrderRepository orderRepository;
 
     @Transactional(propagation = Propagation.REQUIRED)
